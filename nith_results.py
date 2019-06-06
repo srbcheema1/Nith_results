5e5e27e34e3819a27736c323e3c26a6911c2b3fb
Sarbjit Singh <srbcheema1@gmail.com>
2019-06-06 20:40:35 +0530

    2019 may result

--- nith_results.py
+++ nith_results.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
 
-from nith_result import *
-
 if __name__ == "__main__":
-    from nith_result.main import main
+    from nith_results.main import main
     main()
